b3d52abfb7abafa43e1622762b0ad123b4a6adc5
oroszdd <oroszdd@gmail.com>
2018-07-02 19:08:19 +0200

    Initial

--- app/src/main/java/com/example/orosz/fidlee/MainActivity.java
+++ app/src/main/java/com/example/orosz/fidlee/MainActivity.java
@@ -12,10 +12,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText editText1;
-        editText1 = findViewById(R.id.editText1);
+        final EditText editText1, editText2, editText3, editText4, editText5, editText6, editText7
+                , editText8, editText9, editText10, editText11;
 
-        //Comment
+        editText1 = findViewById(R.id.editText1);
+        editText2 = findViewById(R.id.editText2);
+        editText3 = findViewById(R.id.editText3);
+        editText4 = findViewById(R.id.editText4);
+        editText5 = findViewById(R.id.editText5);
+        editText6 = findViewById(R.id.editText6);
+        editText7 = findViewById(R.id.editText7);
+        editText8 = findViewById(R.id.editText8);
+        editText9 = findViewById(R.id.editText9);
+        editText10 = findViewById(R.id.editText10);
+        editText11 = findViewById(R.id.editText11);
+
+        
+
+
 
     }
 }
