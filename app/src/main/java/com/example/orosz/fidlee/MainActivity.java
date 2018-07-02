4c8b4461c440c8d28f6871b879bd1092ff2f217d
oroszdd <oroszdd@gmail.com>
2018-07-02 17:34:08 +0200

    Initial

---
+++ app/src/main/java/com/example/orosz/fidlee/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.orosz.fidlee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText editText1;
+        editText1 = findViewById(R.id.editText1);
+
+
+    }
+}
